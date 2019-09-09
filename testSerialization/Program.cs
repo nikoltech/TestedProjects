4be182ef33cc1b51d3eadf1c471aa8b96bb35879
Vitali Nikolenko <nvvnikolenko@gmail.com>
2019-09-09 15:24:51 +0300

    Upload to branch

---
+++ testSerialization/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Xml;
+
+namespace testSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Tst tst = new Tst();
+            tst.Name = "My name";
+            tst.SecondValue = "My second value";
+
+            Person pers = new Person();
+            pers.Name = "My p name";
+            pers.SecondValue = "My p second value";
+            DataContractSerializer ds = new DataContractSerializer(typeof(Person));
+            using (Stream xmlWriter = File.Create("dataContractWriterTst.xml"))
+            {
+                ds.WriteObject(xmlWriter, pers);
+            }
+            Person pers2;
+            using (Stream xmlWriter = File.OpenRead("dataContractWriterTst.xml"))
+            {
+                pers2 = (Person)ds.ReadObject(xmlWriter);
+            }
+            Console.WriteLine(pers2.Name + ", " + pers2.SecondValue);
+
+            tst.p = pers;
+            tst.p2 = pers;
+            DataContractSerializerSettings set = new DataContractSerializerSettings { PreserveObjectReferences = true };
+            DataContractSerializer ds2 = new DataContractSerializer(typeof(Tst), set);
+            XmlWriterSettings xmlWriterSettings = new XmlWriterSettings();
+            using (XmlWriter xmlWriter = XmlWriter.Create("xmlWriterTst.xml", xmlWriterSettings))
+            {
+                ds2.WriteObject(xmlWriter, tst);
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+
+    [Serializable] class Tst //: ISerializable
+    {
+        public string Name;
+        public string SecondValue;
+        [DataMember]public Person p;
+        [DataMember]public Person p2;
+
+        public virtual void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", this.Name);
+            info.AddValue("SecondValue", this.SecondValue);
+        }
+
+        [OnSerializing] void CreatePersonIfNull(StreamingContext ctx)
+        {
+            this.p = this.p ?? new Person { Name = "Default pname", SecondValue = "Default pval" };
+        }
+    }
+
+    [Serializable] class Person
+    {
+        public string Name;
+        public string SecondValue;
+    }
+}
