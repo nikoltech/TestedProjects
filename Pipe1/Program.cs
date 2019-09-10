48fe13b9d00779629febe7532f9bb3cd95a80bd2
Vitali Nikolenko <nvvnikolenko@gmail.com>
2019-09-10 23:06:19 +0300

    Добавьте файлы проекта.

---
+++ Pipe1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO.Pipes;
+
+namespace Pipe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello server Pipe1!");
+
+            using (var s = new NamedPipeServerStream("pipestream"))
+            {
+                s.WaitForConnection();
+                s.WriteByte(100);
+                Console.WriteLine(s.ReadByte());
+            }
+
+            int[][] mass = new int[5][];
+
+            Console.ReadKey();
+        }
+    }
+}
