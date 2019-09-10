9711f29269030c024c47d729fa693fe3b9ad1493
Vitali Nikolenko <nvvnikolenko@gmail.com>
2019-09-10 19:47:09 +0300

    Добавьте файлы проекта.

---
+++ Pipe2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO.Pipes;
+
+namespace Pipe2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello client Pipe2!");
+
+            using (var s = new NamedPipeClientStream("pipestream"))
+            {
+                s.Connect();
+                Console.WriteLine(s.ReadByte());
+                s.WriteByte(200);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
