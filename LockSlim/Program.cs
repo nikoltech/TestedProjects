58e318b4a44638208e6f8ad7333d502256730e54
Vitali Nikolenko <nvvnikolenko@gmail.com>
2019-09-12 17:35:57 +0300

    Add project files.

---
+++ LockSlim/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LockSlim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Slim s = new Slim();
+            s.StartThreads();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LockSlim/Slim.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace LockSlim
+{
+    /// <summary>
+    /// class Slim is sync context
+    /// </summary>
+    public class Slim
+    {
+        private ReaderWriterLockSlim _rw = new ReaderWriterLockSlim();
+        private List<int> _items = new List<int>();
+        private Random _rand = new Random();
+
+        public void StartThreads()
+        {
+            new Thread(new ParameterizedThreadStart(Read)).Start(1);
+            new Thread(new ParameterizedThreadStart(Read)).Start(2);
+            new Thread(new ParameterizedThreadStart(Read)).Start(3);
+            new Thread(new ParameterizedThreadStart(Write)).Start(new WriteObject { id = 4, threadId = "A" });
+            new Thread(new ParameterizedThreadStart(Write)).Start(new WriteObject { id = 5, threadId = "B" });
+        }
+
+        private void Read(object idObj)
+        {
+            int id = (int)idObj;
+
+            while (true)
+            {
+                this._rw.EnterReadLock();
+                foreach (int i in this._items)
+                {
+                    Thread.Sleep(10);
+                    Console.WriteLine($"Read #{id} -  {i}");
+                }
+                this._rw.ExitReadLock();
+            }
+        }
+
+        private void Write(object obj)
+        {
+            int id = ((WriteObject)obj).id;
+            object threadId = ((WriteObject)obj).threadId;
+
+            while (true)
+            {
+                int number = this.GetRandNum(100);
+                this._rw.EnterWriteLock();
+                this._items.Add(number);
+                this._rw.ExitWriteLock();
+                Console.WriteLine($"Write #{id} - Thread {threadId} added {number}");
+                Thread.Sleep(100);
+            }
+        }
+
+        private class WriteObject
+        {
+            public int id;
+            public object threadId;
+        }
+
+        private int GetRandNum(int max)
+        {
+            lock (this._rand)
+            {
+                return this._rand.Next(max);
+            }
+        }
+    }
+}
