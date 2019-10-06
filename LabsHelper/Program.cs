68d81a1130336d5bbab380231e06d54758577658
Vitali Nikolenko <nvvnikolenko@gmail.com>
2019-10-06 13:29:47 +0300

    Add project files.

---
+++ LabsHelper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabsHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
