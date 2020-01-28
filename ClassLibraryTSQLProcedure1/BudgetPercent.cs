26f0ef384fed053090d40db73c4a05db4a6e7888
Vitali Nikolenko <nvvnikolenko@gmail.com>
2020-01-28 14:42:38 +0200

    BudgetPercent procedure code

---
+++ ClassLibraryTSQLProcedure1/BudgetPercent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassLibraryTSQLProcedure1
+{
+    using System.Data.SqlTypes;
+
+    public class BudgetPercent
+    {
+        private const float percent = 12;
+
+        public static SqlDouble ComputeBudget(float budget)
+        {
+            return budget * percent;
+        }
+    }
+}
--- ClassLibraryTSQLProcedure1/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ClassLibraryTSQLProcedure1
-{
-    public class Class1
-    {
-    }
-}
