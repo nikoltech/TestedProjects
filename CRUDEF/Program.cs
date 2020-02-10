7e2d3b63c971708aed9c3f0ca8af2f5c2c660836
Vitali Nikolenko <nvvnikolenko@gmail.com>
2020-02-10 19:29:07 +0200

    EF entry

--- CRUDEF/Program.cs
+++ CRUDEF/Program.cs
@@ -1,8 +1,13 @@
 ﻿namespace CRUDEF
 {
     using CRUDEF.Entities;
+    using CRUDEF.Providers;
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Infrastructure;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
     using System;
+    using System.IO;
     using System.Linq;
 
     public class Program
@@ -10,10 +15,20 @@
         
         public static void Main(string[] args)
         {
+            var builder = new ConfigurationBuilder();
+            // установка пути к текущему каталогу
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            // получаем конфигурацию из файла appsettings.json
+            builder.AddJsonFile("appsettings.json");
+            // создаем конфигурацию
+            var config = builder.Build();
+            // получаем строку подключения
+            string connectionString = config.GetConnectionString("DefaultConnection");
+
             var optionsBuilder = new DbContextOptionsBuilder<AppContext>();
 
             var options = optionsBuilder
-                    .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;")
+                    .UseSqlServer(connectionString)
                     .Options;
 
             // Добавление
@@ -82,6 +97,24 @@
                 }
 
                 _ = db.Database.ExecuteSqlCommand("TRUNCATE TABLE Users");
+            }
+
+            using (AppContext db = new AppContext(options))
+            {
+                //db.GetService<ILoggerFactory>().AddProvider(new crudefDbLoggerProvider());
+                User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Alice", Age = 26 };
+
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+
+                var users = db.Users.ToList();
+                Console.WriteLine("Данные после добавления:");
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
             }
             Console.Read();
         }
---
+++ CRUDEF/Providers/crudefDbLoggerProvider.cs
@@ -0,0 +1,47 @@
+﻿namespace CRUDEF.Providers
+{
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.IO;
+
+    public class crudefDbLoggerProvider : ILoggerProvider
+    {
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new MyLogger();
+        }
+
+        public void Dispose()
+        { }
+
+        private class MyLogger : ILogger
+        {
+            public IDisposable BeginScope<TState>(TState state)
+            {
+                return null;
+            }
+
+            public bool IsEnabled(LogLevel logLevel)
+            {
+                return true;
+                //switch (logLevel)
+                //{
+                //    case LogLevel.Information:
+                //    //case LogLevel.Debug:
+                //    case LogLevel.Error:
+                //    case LogLevel.Warning:
+                //        return true;
+                //}
+
+                //return false;
+            }
+
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+            {
+                string contents = formatter(state, exception);
+                File.AppendAllText("log.txt", contents);
+                Console.WriteLine(contents);
+            }
+        }
+    }
+}
