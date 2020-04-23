7b281629d1cf78ce7bb6bb3c7e2ad27e7438c537
Vitali Nikolenko <nvvnikolenko@gmail.com>
2020-04-23 16:55:15 +0300

    Add project files.

---
+++ WebApp1/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApp1.Entities;
+
+namespace WebApp1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebApp1.Entities.Test> Test { get; set; }
+    }
+}
---
+++ WebApp1/Entities/Test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace WebApp1.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Test
+    {
+        [Key]
+        public string TestId {get; set;}
+    }
+}
